e144b080fba63ba6dbd6c957daf153e6529e7d1e
tutqq96 <tutqq96@gmail.com>
2024-09-15 04:37:00 +0700

    feat(footer): Update footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -26,13 +26,13 @@ const Footer = () => {
 
 						<div className="col-6 col-md-6 col-lg-3">
 							<div className="footer-widget footer-menu">
-								<h2 className="footer-title">For Patients</h2>
+								<h2 className="footer-title">For Customers</h2>
 								<ul>
-									<li><Link to={'/doctors'}><FaAngleDoubleRight className='icon' />  Search for Doctors</Link></li>
+									<li><Link to={'/cars'}><FaAngleDoubleRight className='icon' />  Search for Cars</Link></li>
 									<li><Link to={'/login'}><FaAngleDoubleRight className='icon' />  Login</Link></li>
 									<li><Link to={'/login'}><FaAngleDoubleRight className='icon' />  Register</Link></li>
-									<li><Link to={'/doctors'}><FaAngleDoubleRight className='icon' />  Booking</Link></li>
-									<li><Link to={'/'}><FaAngleDoubleRight className='icon' />  Patient Dashboard</Link></li>
+									<li><Link to={'/cars'}><FaAngleDoubleRight className='icon' />  Renting</Link></li>
+									<li><Link to={'/'}><FaAngleDoubleRight className='icon' />  Customer Dashboard</Link></li>
 								</ul>
 							</div>
 						</div>
@@ -40,12 +40,12 @@ const Footer = () => {
 						<div className="col-6 col-md-6 col-lg-3">
 
 							<div className="footer-widget footer-menu">
-								<h2 className="footer-title">For Doctors</h2>
+								<h2 className="footer-title">For Admin</h2>
 								<ul>
 									<li><Link to={'/'}><FaAngleDoubleRight className='icon' /> Appointments</Link></li>
 									<li><Link to={'/login'}><FaAngleDoubleRight className='icon' /> Login</Link></li>
 									<li><Link to={'/register'}><FaAngleDoubleRight className='icon' /> Register</Link></li>
-									<li><Link to={'/dashboard'}><FaAngleDoubleRight className='icon' /> Doctor Dashboard</Link></li>
+									<li><Link to={'/dashboard'}><FaAngleDoubleRight className='icon' /> Admin Dashboard</Link></li>
 								</ul>
 							</div>
 						</div>
@@ -56,15 +56,15 @@ const Footer = () => {
 								<div className="footer-contact-info">
 									<div className="footer-address">
 										<span><i className="fas fa-map-marker-alt"></i></span>
-										<p> 121, Mirzapure Union office,<br /> Sylhet, Bangladesh 03214 </p>
+										<p> 02 Võ Oanh, Phường 25, Bình Thạnh, <br /> Hồ Chí Minh, Vietnam </p>
 									</div>
 									<p>
 										<i className="fas fa-phone-alt"></i>
-										(+84)981 875 370
+										(+84) 981 875 370
 									</p>
 									<p className="mb-0">
 										<i className="fas fa-envelope"></i>
-										ujjalzaman@gmail.com
+										2231123101@gmail.com
 									</p>
 								</div>
 							</div>
@@ -72,33 +72,6 @@ const Footer = () => {
 					</div>
 				</div>
 			</div>
-			<div className="footer-bottom">
-				<div className="container-fluid">
-
-					<div className="copyright">
-						<div className="row">
-							<div className="col-md-6 col-lg-6">
-								<div className="copyright-text">
-									<p className="mb-0"><a href="templateshub.net">
-										<div className="copyRight text-center">
-											<p>Copyright {(new Date()).getFullYear()} All Rights Reserved</p>
-										</div></a></p>
-								</div>
-							</div>
-							<div className="col-md-6 col-lg-6">
-								<div className="copyright-menu">
-									<ul className="policy-menu d-flex gap-2 justify-content-center">
-										<Link to={'/'} className='text-white'>Terms and Conditions</Link>
-										<Link to={'/'} className='text-white'>Policy</Link>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
 		</footer>
 	);
 };
